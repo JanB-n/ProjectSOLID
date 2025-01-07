92a1e0cf4be05b7ed5990b539988001533b0e387
JanB-n <janbaran975@gmail.com>
2025-01-07 22:06:28 +0100

    last small changes

--- src/main/java/project/Application.java
+++ src/main/java/project/Application.java
@@ -5,21 +5,32 @@ import project.DTO.Parameters;
 public class Application {
     public static void main(String[] args) {
         InputParser inputParser = new InputParser();
-        TimeZoneManager timeZoneManager = new TimeZoneManager();
         InputReader inputReader = new InputReader(inputParser);
+
+        TimeZoneManager timeZoneManager = new TimeZoneManager();
         InputValidator inputValidator = new InputValidator(timeZoneManager);
         TimeConverter timeConverter = new TimeConverter(timeZoneManager);
+
         ResponsePrinter responsePrinter = new ResponsePrinter();
+
         while (true) {
             inputReader.loadUserInput();
+
             if (inputReader.checkForExit())
                 break;
-            Parameters parameters = inputReader.getParameters();
-            boolean validParams = inputValidator.validate(parameters);
-            if (validParams) {
-                Parameters newTime = timeConverter.convert(parameters);
-                responsePrinter.printTime(newTime);
+
+            try {
+                Parameters parameters = inputReader.getParameters();
+                boolean validParams = inputValidator.validate(parameters);
+                if (validParams) {
+                    Parameters newTime = timeConverter.convert(parameters);
+                    responsePrinter.printTime(newTime);
+                }
             }
+            catch (IllegalArgumentException e) {
+                System.out.println("Bad argument format");
+            }
+
         }
     }
 }
